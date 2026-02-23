4b887104807b5e279c98ae32a555e85bcc97a7b5
ahanafintesher <ahanafintesher01@gmail.com>
2026-02-23 23:21:59 +0600

    interrview section functionallity added

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let allCardSection = document.getElementById('allCards');
 
 let mainContainer = document.querySelector('main');
 
+const interviewSection = document.getElementById('interview-section');
+
 function calculateCount(){
     total.innerText = allCardSection.children.length;
     jobCount.innerText =  allCardSection.children.length;
@@ -37,19 +39,80 @@ function toogleStyle(id){
    selected.classList.remove('bg-white' , 'text-[#64748B]')
    selected.classList.add('bg-[#3B82F6]' , 'text-white')
 
+   if(id == 'interview-filter-btn'){
+    allCardSection.classList.add('hidden')
+    interviewSection.classList.remove('hidden')
+   }
+   else if(id == 'all-filter-btn'){
+    allCardSection.classList.remove('hidden')
+    interviewSection.classList.add('hidden')
+   }
+
   
 }
 
 mainContainer.addEventListener('click', function(event){
-    const parentNode = event.target.parentNode.parentNode;
-    const jobName = parentNode.querySelector('.job-name').innerText;
-    const jobPostName = parentNode.querySelector('.job-post-name').innerText;
-    const jobRequirements = parentNode.querySelector('.job-requirements').innerText;
-    const status = parentNode.querySelector('.status').innerText;
-    const notes = parentNode.querySelector('.notes').innerText;
+    console.log(event.target.classList.contains('interview-btn'))
+    if(event.target.classList.contains('interview-btn')){
+        const parentNode = event.target.parentNode.parentNode;
+        const jobName = parentNode.querySelector('.job-name').innerText;
+        const jobPostName = parentNode.querySelector('.job-post-name').innerText;
+        const jobRequirements = parentNode.querySelector('.job-requirements').innerText;
+        const status = parentNode.querySelector('.status').innerText;
+        const notes = parentNode.querySelector('.notes').innerText;
+         parentNode.querySelector('.status').innerText = 'Interview'
 
-    
+        const cardInfo = {
+            jobName,
+            jobPostName,
+            jobRequirements,
+            status:'interview',
+            notes
+        }
 
+        const jobExist = interviewList.find(item => item.jobName == cardInfo.jobName)
 
+       
+
+        if(!jobExist){
+            interviewList.push(cardInfo)
+        }
+
+    calculateCount();
+    renderInterview()
+    }
    
+
 })
+
+function renderInterview(){
+    interviewSection.innerHTML = '';
+
+    for(let interview of interviewList){
+        let div = document.createElement('div')
+        div.className = 'card flex justify-between p-6 shadow mb-4 '
+        div.innerHTML = `
+         <div class="space-y-6">
+                    <div>
+                        <p class="job-name font-semibold text-[22px]">${interview.jobName}</p>
+                        <p class="job-post-name  text-[#64748B]">React Native Developer</p>
+                    </div>
+                    <div>
+                        <p class="job-requirements text-[#64748B]"> Remote • Full-time • $130,000 – $175,000</p>
+                    </div>
+                    <div>
+                        <p class="status bg-[#EEF4FF] inline px-3 py-2 font-medium">${interview.status}</p>
+                    </div>
+                    <div>
+                        <p class="notes">Build cross-platform mobile applications using React Native. Work on products used by
+                            millions of users worldwide.</p>
+                    </div>
+                    <div class="flex gap-2">
+                        <button class="interview-btn font-semibold border-2 px-3 py-2 text-[#10B981] rounded-sm">INTERVIEW</button>
+                        <button class="rejected-btn font-semibold border-2 px-3 py-2 text-[#EF4444] rounded-sm">REJECTED</button>
+                    </div>
+         </div>      
+        `
+        interviewSection.appendChild(div)
+    }
+}
